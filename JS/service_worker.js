725dc20aeb559858c3696dff43b599197bc7dd07
louise <louise.decombe@laplateforme.io>
2022-05-30 16:31:32 +0200

    add install and activate event version / add offline html file

--- JS/service_worker.js
+++ JS/service_worker.js
@@ -1,23 +1,40 @@
-self.addEventListener('fetch', (event) => {
-    console.log(`Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
-if (event.request.mode == "navigate") {
-    event.respondWith(
-        (async () => {
+const PREFIX = "Version";
 
-            try {
-            const preloadResponse = await event.preloadResponse
-            if (preloadResponse){
-                return preloadResponse
-            }
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+    event.waitUntil((async () => {
+        const cache = await caches.open(PREFIX)
+        cache.add(new Request('/offline.html'))
+        
+    })());
 
-          return await fetch(event.request)
+    console.log(`${PREFIX} Install`);
 
-        } catch(e){
-            return new Response("Error");
+});
+self.addEventListener("activate", () => {
+    clients.claim();
+    console.log(`${PREFIX} Active`);
+});
 
-        }
-        } )()
-    );
-}
+self.addEventListener("fetch", (event) => {
+    console.log(`${PREFIX} Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
+    if (event.request.mode == "navigate") {
+        event.respondWith(
+            (async () => {
 
+                try {
+                    const preloadResponse = await event.preloadResponse
+                    if (preloadResponse) {
+                        return preloadResponse
+                    }
+
+                    return await fetch(event.request)
+
+                } catch (e) {
+                    return new Response("Error");
+
+                }
+            })()
+        );
+    }
 });
