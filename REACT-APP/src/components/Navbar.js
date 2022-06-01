de318417ba552cf7ed60c98055b57f4e22d2eef6
louise <louise.decombe@laplateforme.io>
2022-06-01 15:36:09 +0200

    del default app settings

---
+++ REACT-APP/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+export default function Navbar() {
+    return (
+      <nav className="navigation">
+        <a href="/" className="brand-name">
+          App
+        </a>
+ 
+        <div
+          className="navigation-menu">
+          <ul>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
