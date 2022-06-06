cf5ad22065ff505bd571080444d8f8fde63e05e0
louise <louise.decombe@laplateforme.io>
2022-06-06 11:20:41 +0200

    fix password database name

--- REACT-APP/backend/config/database.js
+++ REACT-APP/backend/config/database.js
@@ -15,7 +15,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', ''),
       user: env('DATABASE_USERNAME', ''),
-      password: env('MYSQL_ROOT_PASSWORD', ''),
+      password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
