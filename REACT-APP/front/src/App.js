ea3952a78354ce07404f081352db12fed8f530ed
louise <louise.decombe@laplateforme.io>
2022-06-08 16:34:46 +0200

    fix app menu icon

--- REACT-APP/front/src/App.js
+++ REACT-APP/front/src/App.js
@@ -14,7 +14,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
